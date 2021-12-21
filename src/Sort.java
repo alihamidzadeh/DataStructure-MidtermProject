5b7292d4dcfe02114b2db6d18f311929f6440340
Ali <ahamidzadeh93@gmail.com>
2021-12-21 13:21:23 +0330

    Add BubbleSort, Recursive Bubble Sort, Selection Sort, Insertion Sort, Insertion Sort Reverse, Recursive Insertion Sort methods in Sort Class.

--- src/Sort.java
+++ src/Sort.java
@@ -1,7 +1,59 @@
+import java.util.Arrays;
+
 public class Sort {
-//    public Sort(int type, int[] array){
-//
-//    }
+
+    private static int[] BubbleSort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (array[j] < array[i]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+            System.out.println(Arrays.toString(array));
+        }
+        return array;
+    }
+
+    private static int[] RecursiveBubbleSort(int[] array, int n) {
+        if (n == 1)
+            return array;
+        else {
+            for (int i = 0; i < n - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                }
+            }
+            System.out.println(Arrays.toString(array));
+            RecursiveBubbleSort(array, n - 1);
+        }
+        return array;
+    }
+
+    private static int[] SelectionSort(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            int min = array[i];
+            int index = 0;
+            for (int j = i + 1; j < array.length; j++)
+                if (array[j] < min) {
+                    min = array[j];
+                    index = j;
+                }
+
+            if (min < array[i]) {
+                int temp = array[i];
+                array[i] = min;
+                array[index] = temp;
+            }
+            System.out.println(Arrays.toString(array));
+        }
+        return array;
+    }
 
     private static int[] InsertionSort(int[] array) {
         for (int j = 1; j < array.length; j++) {
@@ -13,9 +65,7 @@ public class Sort {
             }
             array[i + 1] = key;
 
-            for (int q = 0; q < array.length; q++)
-                System.out.print(array[q] + " ");
-            System.out.println();
+            System.out.println(Arrays.toString(array));
         }
 
         return array;
@@ -31,55 +81,47 @@ public class Sort {
                 array[i - 1] = yoyo;
                 i++;
             }
-            for (int q = 0; q < array.length; q++)
-                System.out.print(array[q] + " ");
-            System.out.println();
-
+            System.out.println(Arrays.toString(array));
         }
         return array;
     }
 
-//    private static int[] ReverseArray(int[] array) {
-//        int j = 0;
-//
-//        for (int q = 0; q < array.length; q++)
-//            System.out.print(array[q] + " ");
-//        System.out.println();
-//
-//        for (int i = array.length-1; i >= array.length / 2; i--) {
-//            int temp = array[i];
-//            array[i] = array[j];
-//            array[j] = temp;
-//            j++;
-//
-//            for (int q = 0; q < array.length; q++)
-//                System.out.print(array[q] + " ");
-//            System.out.println();
-//        }
-//
-//        return array;
-//    }
+    private static int[] RecursiveInsertionSort(int[] array, int n) {
+        if (n <= 1)
+            return array;
+        else {
+            RecursiveInsertionSort(array, n - 1);
 
-    private static int[] SelectionSort(int[] array){
-        for (int i = 0 ; i < array.length; i++){
-            int min = array[i];
-            int index = 0;
-            for (int j = i+1; j < array.length;j++){
-                if (array[j]<min) {
-                    min = array[j];
-                    index = j;
-                }
-            }
-            if (min < array[i]) {
-                int temp = array[i];
-                array[i] = min;
-                array[index] = temp;
-            }
+            int last = array[n - 1];
+            int j = n - 2;
 
-            for (int q = 0; q < array.length; q++)
-                System.out.print(array[q] + " ");
-            System.out.println();
+            while (j >= 0 && array[j] > last) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = last;
+            System.out.println(Arrays.toString(array));
         }
         return array;
+    }
+
+
+    //****************************************
+
+
+
+    //*****************************************************
+    public static void main(String[] args) {
+        int arr[] = {64, 34, 25, 12, 22, 11, 90, -2, 0};
+        System.out.println(Arrays.toString(arr));
+        System.out.println("*******************************************");
+//        int[] b = BubbleSort(arr);
+//        int[] b = RecursiveBubbleSort(arr, arr.length);
+//        int[] b = SelectionSort(arr);
+//        int[] b = InsertionSort(arr);
+//        int[] b = InsertionSortReverse(arr);
+        int[] b = RecursiveInsertionSort(arr, arr.length);
+        System.out.println("*******************************************");
+        System.out.println(Arrays.toString(b));
     }
 }
